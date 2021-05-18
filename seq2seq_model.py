ee413ee06a21bb15ed9bea77a0044da4c9ff4690
gullerg <gustavmgrimberg@gmail.com>
2021-05-18 17:08:13 +0100

    decoder and seq2seq model added

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.nn.modules import dropout
+from torch.nn.modules import dropout, module
 import torch.optim as optim
 from torchtext.legacy.datasets import Multi30k
 from torchtext.legacy.data import Field, BucketIterator
@@ -46,8 +46,113 @@ class Encoder(nn.Module):
         return hidden, cell
 
 class Decoder(nn.Module):
-    pass
+    def __init__(self, input_size, embedding_size, hidden_size, output_size, num_layers, p):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.dropout = nn.Dropout(p)
+        self.embedding = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, hidden, cell):
+        x = x.unsqueeze(0)
+
+        embedding = self.dropout(self.embedding(x))
+
+        outputs, _ = self.rnn(embedding, (hidden, cell))    
+
+        predictions = self.fc(outputs)
+
+        predictions = predictions.squeeze(0)
+
+        return predictions, hidden, cell
 
 class Seq2Seq(nn.Module):
-    pass
+    def __init__(self, encoder, decoder):
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, source, target, teacher_force_ratio=0.5):
+        batch_size = source.shape[1]
+        target_len = target.shape[0]
+        target_vocab_size = len(english.vocab)
+
+        outputs = torch.zeros(target_len, batch_size, target_vocab_size).to(device)
+
+        hidden, cell = self.encoder(source)
+
+        x =  target[0]
+
+        for t in range(1, target_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+
+            outputs[t] = output
+
+            best_guess = output.argmax(1)
+
+            x = target[t] if random.random() < teacher_force_ratio else best_guess
+        return outputs
+
+num_epochs = 20
+learning_rate = 0.001
+batch_size = 64
+
+load_model = False
+device = torch.device('gpu' if torch.cuda.is_available() else 'cpu')
+input_size_encoder = len(german.vocab)
+input_size_decoder = len(english.vocab)
+output_size = len(english.vocab)
+encoder_embedding_size = 300
+decoder_embedding_size = 300
+hidden_size = 1024
+num_layers = 2
+enc_dropout = 0.5
+dec_dropout = 0.5
+
+writer = SummaryWriter(f'runs/loss_plot')
+steps = 0
+
+train_iterator, valid__iterator, test__iterator = BucketIterator.splits(
+    (train_data, validation_data, test_data),
+    batch_size = batch_size,
+    sort_within_batch = True,
+    sort_key = lambda x: len(x.src)
+)
+
+encoder_net = Encoder(input_size_encoder, encoder_embedding_size, hidden_size, num_layers, enc_dropout)
+decoder_net = Decoder(input_size_decoder, decoder_embedding_size, hidden_size, output_size ,num_layers, dec_dropout)
+
+model = Seq2Seq(encoder_net, decoder_net)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+pad_idx = english.vocab.stoi['<pad>']
+criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+step = 0
+
+for epoch in range(num_epochs):
+    print(f'Epoch {epoch} / {num_epochs}')
+    checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+    
+    for batch_idx, batch in enumerate(train_iterator):
+        inp_data = batch.src.to(device)
+        target = batch.trg.to(device)
+
+        output = model(inp_data, target)
+
+        output = output[1:].reshape(-1, output.shape[2])
+        target = target[1:].reshape(-1)
+
+        optimizer.zero_grad()
+        loss = criterion(output, target)
+        
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+        optimizer.step()
+
+        writer.add_scalar('Training loss', loss, global_step=step)
+        step += 1
 
